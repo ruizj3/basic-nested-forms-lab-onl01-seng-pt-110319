29f00d79f6da71b9c6cc8c52a31e2427d5b5308c
Joseph Ruiz <ruizj3@uw.edu>
2020-04-21 00:18:06 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,27 +1,4 @@
-class RecipesController < ApplicationController
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
-
-  def index
-    @recipes = Recipe.all
-  end
-
-  def new
-    @recipe = Recipe.new
-    @recipe.ingredients.build(name: "First Ingredient")
-    @recipe.ingredients.build(name: "Second Ingredient")
-  end
-
-  def create
-    Recipe.create(recipe_params)
-    redirect_to recipes_path
-  end
-
-  def recipe_params
-    params.require(:recipe).permit(
-      :title,
-      ingredients_attributes: [ :name, :quantity ]
-    )
-  end
+class Recipe < ActiveRecord::Base
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
 end
